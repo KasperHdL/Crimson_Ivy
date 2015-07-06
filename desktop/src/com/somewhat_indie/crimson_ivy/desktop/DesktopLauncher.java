33403ac235e25ca85aed6aef67ccc37532f0a615
KasperHdL <kasper@honnens.dk>
2015-07-06 16:58:44 +0200

    initial commit

---
+++ desktop/src/com/somewhat_indie/crimson_ivy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.somewhat_indie.crimson_ivy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.somewhat_indie.crimson_ivy.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
